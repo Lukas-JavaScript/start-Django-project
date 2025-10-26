60925b1a61fcd1e03bb0b6c40b6744f02f5efa94
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-26 12:25:49 +0100

    use git?

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import os
 project_name = input("Whats your Project name?: ")
 app_name = input("Whats your app name?: ")
-git = input("Do you want to use git?(y/n) (default: y)")
+git = input("Do you want to initialize a git repository? (y/n) (default: y): ").lower()
+
+os.mkdir(project_name)
+os.chdir(project_name)
+if git == '':
+    git = 'y'
+if git == 'y':
+    os.system('git init')
+os.system(f"django-admin startproject {project_name}")
 
